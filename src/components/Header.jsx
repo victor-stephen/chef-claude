a94328d4d76a1f7695158a083b71b12524c4fe88
Victor Stephen <83133960+victor-stephen@users.noreply.github.com>
2025-02-04 11:37:18 +0000

    implement and style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
+import logo from "../images/chef-claude-icon.png";
 export default function Header() {
   return (
     <header>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <span>Chef Claude</span>
+      <img src={logo} className="logo" alt="chef-claude" />
+      <span className="brand-name">Chef Claude</span>
     </header>
   );
 }
