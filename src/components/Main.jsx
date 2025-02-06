453750314256dca1ba8185e58010c68841a1ace1
Victor Stephen <83133960+victor-stephen@users.noreply.github.com>
2025-02-06 18:53:10 +0000

    fix merge conflict

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export default function Main() {
-  return (
-    <main>
-      <form className="add-ingredient-form">
-=======
 import { useState } from "react";
 
 export default function Main() {
@@ -23,22 +17,15 @@ export default function Main() {
   return (
     <main>
       <form className="add-ingredient-form" onSubmit={handleSubmit}>
->>>>>>> draft/heuristic-fog
         <input
           type="text"
           placeholder="e.g oregano"
           aria-label="Add ingredient"
-<<<<<<< HEAD
-        />
-        <button>Add ingredient</button>
-      </form>
-=======
           name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
       <ul id="ingredients-list">{ingredientsListItems}</ul>
->>>>>>> draft/heuristic-fog
     </main>
   );
 }
